51afb1d37b8fc63dc141fcd75dc2c0fb4f500ee0
Daniel Wyatt <daniel.wyatt@henryscheinone.co.nz>
2023-03-06 14:04:07 +1300

    Helper for converting image format to extension

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,22 @@
 class Image < ApplicationRecord
   validates :url, uniqueness: true
+
+  KNOWN_FORMATS = [
+    'image/png',
+    'image/bmp',
+    'image/heic',
+    'image/webp'
+  ]
+
+  def format_to_extension
+    if KNOWN_FORMATS.include? self.format
+      ".#{self.format.split('/').last}"
+    elsif self.format == 'image/jpeg'
+      '.jpg'
+    elsif self.format == 'image/svg+xml'
+      '.svg'
+    else
+      '.jpg'
+    end
+  end
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -7,4 +7,19 @@ class ImageTest < ActiveSupport::TestCase
       Image.create!(url: "test")
     end
   end
+
+  test "format to extension - image/jpeg" do
+    im = Image.new(format: 'image/jpeg')
+    assert_equal im.format_to_extension, '.jpg'
+  end
+
+  test "format to extension - image/png" do
+    im = Image.new(format: 'image/png')
+    assert_equal im.format_to_extension, '.png'
+  end
+
+  test "format to extension - image/unknown" do
+    im = Image.new(format: 'image/unknown')
+    assert_equal im.format_to_extension, '.jpg'
+  end
 end
